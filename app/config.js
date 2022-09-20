2fc8eb6d450c9a688696d97e8c8cf9c93bd645d0
mark-bateman-ch <109281452+mark-bateman-ch@users.noreply.github.com>
2022-09-20 15:16:45 +0100

    Service name change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Update / remove a ROE (prototype)',
 
   // Default port that prototype runs on
   port: '3000',
