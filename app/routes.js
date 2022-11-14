4b88f22cbafaab1321854242743ec634900eb8e8
mark-bateman-ch <109281452+mark-bateman-ch@users.noreply.github.com>
2022-11-14 21:09:06 +0000

    General amends and updates

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,25 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Import V3 routes
-router.use('/v3/', (req, res, next) => {
-  return require(`./views/v3/_routes`)(req, res, next);
-})
-
 // Import V2 routes
 router.use('/v2/', (req, res, next) => {
   return require(`./views/v2/_routes`)(req, res, next);
 })
 
 // Import V1 routes
-router.use('/v1/', (req, res, next) => {
-    return require(`./views/v1/_routes`)(req, res, next);
-  })
+// This has been commented out so as to avoid compilation errors as the v1/_routes file doesn't exist
+//router.use('/v1/', (req, res, next) => {
+//    return require(`./views/v1/_routes`)(req, res, next);
+//  })
+
+// SET GLOBAL PREVIOUS PAGE
+router.use('/', (req, res, next) => {
+  req.session.data.gPreviousLocation = req.get('Referrer');
+  req.session.data.gCurrentLocation = req.originalUrl;
+  console.log('gPreviousLocation was : ' + req.session.data.gPreviousLocation);
+  console.log('gCurrentLocation is   : ' + req.session.data.gCurrentLocation);
+  next();
+});
 
 // GET SPRINT NAME - useful for relative templates
 router.use('/', (req, res, next) => {
--- app/views/v2/_routes.js
+++ app/views/v2/_routes.js
@@ -1,5 +1,79 @@
-const express = require('express')
+const express = require('express');
+const { countBy } = require('lodash');
 const router = express.Router()
+
+// index
+router.post('/index', function(req, res) {
+
+    // Reset all session variables to null (ordered page-by-page)
+
+    // what-is-your-name
+    req.session.data['fullname'] = null;
+
+    // what-is-your-date-of-birth
+    req.session.data['date-of-birth-day'] = null;
+    req.session.data['date-of-birth-month'] = null;
+    req.session.data['date-of-birth-year'] = null;
+
+    // what-is-your-address
+    req.session.data['address-line-1'] = null;
+    req.session.data['address-line-2'] = null;
+    req.session.data['address-town'] = null;
+    req.session.data['address-county'] = null;
+    req.session.data['address-postcode'] = null;
+
+    // contact-details
+    req.session.data['telephone-number'] = null;
+    req.session.data['email'] = null;
+
+    res.redirect('content');
+})
+
+// content
+router.post('/content', function(req, res) {
+    res.redirect('what-is-your-name');
+})
+
+// what-is-your-name
+router.post('/what-is-your-name', function(req, res) {
+    if ((req.session.data.gPreviousLocation).includes('check-your-answers')) {
+        res.redirect('check-your-answers');
+    } else {
+        res.redirect('what-is-your-date-of-birth');
+    }
+})
+
+// what-is-your-date-of-birth
+router.post('/what-is-your-date-of-birth', function(req, res) {
+    if ((req.session.data.gPreviousLocation).includes('check-your-answers')) {
+        res.redirect('check-your-answers');
+    } else {
+        res.redirect('what-is-your-address');
+    }
+})
+
+// what-is-your-address
+router.post('/what-is-your-address', function(req, res) {
+    if ((req.session.data.gPreviousLocation).includes('check-your-answers')) {
+        res.redirect('check-your-answers');
+    } else {
+        res.redirect('contact-details');
+    }
+})
+
+// contact-details
+router.post('/contact-details', function(req, res) {
+    if ((req.session.data.gPreviousLocation).includes('check-your-answers')) {
+        res.redirect('check-your-answers');
+    } else {
+        res.redirect('check-your-answers');
+    }
+})
+
+// check-your-answers
+router.post('/check-your-answers', function(req, res) {
+    res.redirect('confirmation');
+})
 
 // Add your routes here - above the module.exports line
 module.exports = router
